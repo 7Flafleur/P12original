0845b9087b84c16043646a9e6c20a507a5bf09a5
7Flafleur <7nb@hotmail.fr>
2024-05-26 22:07:03 +0200

    sidebar icons

--- Front/react-analytics-dashboard/src/components/Sidebar.jsx
+++ Front/react-analytics-dashboard/src/components/Sidebar.jsx
@@ -1,4 +1,7 @@
-
+import yoga from '../styles/yoga.png';
+import swim from '../styles/swim.png';
+import bike from '../styles/bike.png';
+import dumbbell from '../styles/dumbbell.png';
 
 
 export default function Sidebar(){
@@ -7,11 +10,12 @@ export default function Sidebar(){
     return(
         <aside className="sidebar">
                     <ul>
-                <li><a href="/activity"><i className="icon">🏋️</i></a></li>
-                <li><a href="/swimming"><i className="icon">🏊</i></a></li>
-                <li><a href="/cycling"><i className="icon">🚴</i></a></li>
-                <li><a href="/weight-lifting"><i className="icon">🏋️</i></a></li>
+                <li><a href="/activity"><img src={yoga} alt="" /></a></li>
+                <li><a href="/swimming"><img src={swim} alt="" /></a></li>
+                <li><a href="/cycling"><img src={bike} alt="" /></a></li>
+                <li><a href="/weight-lifting"><img src={dumbbell} alt="" /></a></li>
             </ul>
+            <span className='copyright'>Copyright, SportSee 2020</span>
         </aside>
     )
 }

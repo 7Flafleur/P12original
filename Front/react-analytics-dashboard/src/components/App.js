f8d06e11a788d6657a2d94ddd610a879222065d1
7Flafleur <7nb@hotmail.fr>
2024-05-26 19:47:27 +0200

    Header

---
+++ Front/react-analytics-dashboard/src/components/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      This is my app component
+    </div>
+  );
+}
+
+export default App;
---
+++ Front/react-analytics-dashboard/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">SportSee</div>
+            <nav>
+                <a href="/">Accueil</a>
+                <a href="/profile">Profil</a>
+                <a href="/settings">Réglage</a>
+                <a href="/community">Communauté</a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

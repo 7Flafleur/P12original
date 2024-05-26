f3bbf1ff28b57d4151b8d96cf868ce10136cd745
7Flafleur <7nb@hotmail.fr>
2024-05-26 21:42:31 +0200

    sidebar body

--- Front/react-analytics-dashboard/src/components/App.js
+++ Front/react-analytics-dashboard/src/components/App.js
@@ -1,10 +1,13 @@
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 export default function App() {
   return (
     <div className="App">
+
      
       <Header/>
+      <Sidebar/>
 
     </div>
   );
--- Front/react-analytics-dashboard/src/components/Sidebar.jsx
+++ Front/react-analytics-dashboard/src/components/Sidebar.jsx
@@ -5,8 +5,13 @@ export default function Sidebar(){
 
 
     return(
-        <div className="sidebar">
-
-        </div>
+        <aside className="sidebar">
+                    <ul>
+                <li><a href="/activity"><i className="icon">🏋️</i></a></li>
+                <li><a href="/swimming"><i className="icon">🏊</i></a></li>
+                <li><a href="/cycling"><i className="icon">🚴</i></a></li>
+                <li><a href="/weight-lifting"><i className="icon">🏋️</i></a></li>
+            </ul>
+        </aside>
     )
 }

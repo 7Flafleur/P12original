7640627ddc8db1b7ea13ee969350a71503606fec
7Flafleur <7nb@hotmail.fr>
2024-05-26 18:23:15 +0200

    homepage message

--- Front/react-analytics-dashboard/src/App.js
+++ Front/react-analytics-dashboard/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is my Project 12
         </a>
       </header>
     </div>

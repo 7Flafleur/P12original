3f483c049519a872e68c85dfa54e72d1671add10
7Flafleur <7nb@hotmail.fr>
2024-06-05 16:19:23 +0200

    referencearea

--- Front/react-analytics-dashboard/src/components/Averagesessions.jsx
+++ Front/react-analytics-dashboard/src/components/Averagesessions.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 
-import React from 'react';
 import {
   LineChart,
   Line,
   XAxis,
   YAxis,
   Tooltip,
+  ReferenceArea,
   ResponsiveContainer,
 } from 'recharts';
+
+let xValue=null;
 
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
+    // xValue=payload[0].day;
     return (
       <div className="custom-tooltip" style={{ backgroundColor: '#fff', color: '#000', padding: '5px', border: '1px solid #ccc' }}>
         <p className="label">{`${payload[0].value} min`}</p>
@@ -26,6 +30,22 @@ const CustomTooltip = ({ active, payload, label }) => {
 
 
 export default function Averagesessions(props){
+
+
+  const [activeIndex,setActiveindex] = useState(null)
+
+  function MouseOverState(state){
+     if(state.isTooltipActive){
+      const index = sessions.findIndex(session => session.day === state.activeLabel);
+
+      setActiveindex(index)
+     }
+     else{
+      setActiveindex(null)
+     }
+ 
+  }
+
 
 console.log("Session props:", props.sessions)
 
@@ -63,24 +83,38 @@ const minVal = Math.min(...sessions.map(item => item.sessionLength));
 const maxVal = Math.max(...sessions.map(item => item.sessionLength));
 
 
-
+console.log("active index ",activeIndex )
 
 
     return(
 
+      
         <div className="averagesessions">
         <ResponsiveContainer width="100%" height="90%">
         <p className='durée'>Durée moyenne des séances</p>
 
-          <LineChart
-            data={sessions} margin={{ top: -20, right: 0, left: 0, bottom: 0 }}
-       
+          <LineChart width="100%" height="100%"
+            data={sessions} 
+            onMouseMove={MouseOverState}
+            onMouseLeave={() => setActiveindex(null)}
           >
          
          <XAxis dataKey="day" axisLine={false} stroke='#fff' opacity={0.7} tickLine={false} tickMargin={-20} padding={{ left: 10, right: 10 }} />
             <YAxis domain={[minVal - 20, maxVal + 20]} hide={true} />
             <Tooltip content={<CustomTooltip  />} />  
             <Line type="monotone" dataKey="sessionLength" stroke="#ffffff" dot={{ r: 0 }} activeDot={{ r: 3 }} />
+            
+              <ReferenceArea className='referencearea'
+                x1={activeIndex}
+                x2={6}
+                strokeOpacity={0.8}
+           
+                
+                
+               
+                fill="#979797"
+              />
+            
           </LineChart>
         </ResponsiveContainer>
       </div>
